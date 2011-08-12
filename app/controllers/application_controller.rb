0dcb4cbe0341a01877b01fca4836b562b00b309d
Jason MacDonald <jason@jasonmacdonald.(none)>
2011-08-11 17:22:33 -0700

    Added proper caching of variables for application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,34 +7,25 @@ class ApplicationController < ActionController::Base
 
   private
 
-  before_filter :instantiate_controller_and_action_names
-  def instantiate_controller_and_action_names
-    @action_name = action_name
-    @controller_name = controller_name
-  end
-
   def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    @@current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
   
   def fb_user
-    @fb_user ||= FbGraph::User.me(session[:token]).fetch if session[:token]
+    @@fb_user ||= FbGraph::User.me(session[:token]).fetch if session[:token]
   end
 
   def friends
-    if @friends.nil?
+    if @@friends.nil?
       fb_friends = fb_user.friends if fb_user
       fb_friends_uids = []
       fb_friends.each do |f|
         fb_friends_uids.push(f.identifier)
       end
-      @friends = User.where(:uid => fb_friends_uids).all
-      @friends.sort!{ |f1, f2| f1.name <=> f2.name }
-      puts "@@friends is nil!!"
-    else
-      puts "@@friends is not full!!"
+      @@friends = User.where(:uid => fb_friends_uids).all
+      @@friends.sort!{ |f1, f2| f1.name <=> f2.name }
     end
-    @friends
+    @@friends
   end
 
 end
