667366bdf88f2acf04090d04b24af9c448b64b42
Jason MacDonald <jason@jasonmacdonald.(none)>
2011-08-19 02:10:44 -0700

    Logging user in saved imported schedule.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,11 +3,29 @@ require 'net/https'
 
 class SessionsController < ApplicationController
   def show
-     auth = request.env['omniauth.auth']
-     user = User.find_by_uid(auth['uid']) || User.create_with_omniauth(auth)
+    auth = request.env['omniauth.auth']
+    user = User.find_by_uid(auth['uid']) || User.create_with_omniauth(auth)
     
     session[:user_id] = user.id
     session[:token] = auth['credentials']['token']
+
+		# Updated the imported schedule with the correct user id, and make it active
+		if session[:imported]
+			imported_schedule = Schedule.find(session[:imported])
+			imported_schedule.update_attribute(:user_id, user.id)
+			imported_schedule.save
+			
+			# Find the current user's active schedule and update it to be the imported
+			# schedule, or create a new active schedule
+			active_schedule = ActiveSchedule.find_by_user_id(user.id)
+			if active_schedule
+				active_schedule.update_attribute(:schedule_id, imported_schedule.id)
+				active_schedule.save
+			else
+				ActiveSchedule.create(:user_id => user.id, :schedule_id => imported_schedule.id)
+			end
+		end
+		
     redirect_to root_url
   end
 
