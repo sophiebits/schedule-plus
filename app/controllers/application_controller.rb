93ee8b3301cedc8991cc33aa74985ea7779f4c94
Jason MacDonald <jason@JasonMacDonald.local>
2011-08-21 02:47:50 -0700

    Changing from @@ to @ (class -> instance) variables for application_controller.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,31 +5,31 @@ class ApplicationController < ActionController::Base
   
   helper_method :current_user, :fb_user, :friends
 
-	@@current_user = nil
-	@@fb_user = nil
-	@@friends = nil
+	@current_user = nil
+	@fb_user = nil
+	@friends = nil
 
   private
 
   def current_user
-    @@current_user ||= User.find(session[:user_id]) if session[:user_id]
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
   
   def fb_user
-    @@fb_user ||= FbGraph::User.me(session[:token]).fetch if session[:token]
+    @fb_user ||= FbGraph::User.me(session[:token]).fetch if session[:token]
   end
 
   def friends
-    if @@friends.nil?
+    if @friends.nil?
       fb_friends = fb_user.friends if fb_user
       fb_friends_uids = []
       fb_friends.each do |f|
         fb_friends_uids.push(f.identifier)
       end
-      @@friends = User.where(:uid => fb_friends_uids)
-      @@friends.sort!{ |f1, f2| f1.name <=> f2.name }
+      @friends = User.where(:uid => fb_friends_uids)
+      @friends.sort!{ |f1, f2| f1.name <=> f2.name }
     end
-    @@friends
+    @friends
   end
 
 end
