496410e94032bab3a5bf23fcfd5cf1871d0e1e08
Vincent Siao <vincentsiao@gmail.com>
2011-10-23 21:48:17 -0400

    homepage now in static controller, new actions in courses controller

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,2 +1,9 @@
 class CoursesController < ApplicationController
+  def index
+    
+  end
+
+  def show
+    
+  end
 end
--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,5 +1,9 @@
 class StaticController < ApplicationController
 
+  def home
+
+  end
+
   def tos
 
   end
