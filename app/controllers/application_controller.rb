b4f5ebcbf9b1b7dbdabb8954ebfc8d7269a73ea3
Jason MacDonald <jason@JasonMacDonald.local>
2011-10-24 00:20:56 -0400

    Removing current_semester repeat

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,10 +21,6 @@ class ApplicationController < ActionController::Base
 
   private
   
-  def current_semeseter
-    "F11"
-  end
-
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
