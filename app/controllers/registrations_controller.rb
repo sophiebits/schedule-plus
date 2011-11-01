a11a418b6b0bcf0732a6eb462a847b9f014fa7a9
Vincent Siao <vincentsiao@gmail.com>
2011-11-01 01:29:39 -0400

    registrations controller now inherits devise default

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,4 +1,4 @@
-class RegistrationsController < ApplicationController
+class RegistrationsController < Devise::RegistrationsController
   def create
     super
     session[:omniauth] = nil unless @user.new_record?
