113c2b4009c17d7c222736cdd9ade5779b3e34a2
Vincent Siao <vincentsiao@gmail.com>
2011-12-29 04:50:26 -0500

    friends icon, schedule section change ajax optimization

--- app/views/course_selections/update.js
+++ app/views/course_selections/update.js
@@ -1,5 +1,9 @@
 $('#calendar').find('.course<%= @selection.section.course.number %>').remove();
 var course = $('#schedule .course<%= @selection.section.course.number %>');
 course.find('.selected_section').html('<%= @selection.section.letter %>');
-course.find('.sections .selected').removeClass('section');
-
+course.find('.sections .selected').removeClass('selected');
+course.find('.sections #section<%= @selection.section.id %>').addClass('selected');
+Calendar.addCourse(course);
+var days = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday'];
+for (var i = 0; i < days.length; ++i)
+  Calendar.layoutDay($('#main-schedule li.' + days[i] + ' .courses li'));
