20419b03300d069256987a13f44bbb1775a00389
Vincent Siao <vincentsiao@gmail.com>
2012-01-06 23:15:45 -0800

    Importer base

--- app/models/importer.rb
+++ app/models/importer.rb
@@ -1,15 +1,16 @@
 # Semester is Fall 2011
 
-class Legacy::Base < ActiveRecord::Base
-  # self.abstract_class = true
+class OldBase < ActiveRecord::Base
+  self.abstract_class = true
   establish_connection "old"
 end
 
-class Legacy::User < Legacy::Base
+class OldUser < OldBase
   set_table_name 'users'
 
   has_many :schedules,
-           :class_name => "Legacy::Schedule"
+           :class_name => "OldSchedule",
+           :foreign_key => "user_id"
 
   def to_model
     User.new do |u|
@@ -19,13 +20,14 @@ class Legacy::User < Legacy::Base
   end
 end
 
-class Legacy::Schedule < Legacy::Base
+class OldSchedule < OldBase
   set_table_name 'schedules'
   
   belongs_to :user,
-             :class_name => "Legacy::User"
+             :class_name => "OldUser"
   has_many :course_selections,
-           :class_name => "Legacy::CourseSelection"
+           :class_name => "OldCourseSelection",
+           :foreign_key => "schedule_id"
 
   def to_model
     Schedule.new do |s|
@@ -36,22 +38,80 @@ class Legacy::Schedule < Legacy::Base
   end
 end
 
-class Legacy::CourseSelection < Legacy::Base
+class OldCourse < OldBase
+  set_table_name 'courses'
+
+  has_many :scheduled_courses,
+           :class_name => "OldScheduledCourse",
+           :foreign_key => "course_id"
+end
+
+class OldLecture < OldBase
+  set_table_name 'lectures'
+
+  has_many :scheduled_courses,
+           :class_name => "OldScheduledCourse",
+           :foreign_key => "lecture_id"
+end
+
+class OldRecitation < OldBase
+  set_table_name 'recitations'
+
+  has_many :scheduled_courses,
+           :class_name => "OldScheduledCourse",
+           :foreign_key => "recitation_id"
+end
+
+class OldScheduledCourse < OldBase
+  set_table_name 'scheduled_courses'
+
+  has_many :course_selections,
+           :class_name => "OldCourseSelection"
+  belongs_to :course,
+             :class_name => "OldCourse"
+  belongs_to :lecture,
+             :class_name => "OldLecture"
+  belongs_to :recitation,
+             :class_name => "OldRecitation"
+end
+
+class OldCourseSelection < OldBase
   set_table_name 'course_selections'
 
   belongs_to :schedule,
-             :course_name => "Legacy::Schedule"
+             :class_name => "OldSchedule"
+  belongs_to :scheduled_course,
+             :class_name => "OldScheduledCourse"
 
   def to_model
+    course = Course.find_by_number(self.scheduled_course.course.number)
+    if course.nil?
+      p "Error: Course Not Found (" + self.scheduled_course.course.number + ")"
+      return nil
+    end
+    letter = self.scheduled_course.recitation.try(:section) ||
+             self.scheduled_course.lecture.try(:section)
+    section = course.find_by_section(letter) if letter
     CourseSelection.new do |cs|
       cs.schedule_id = self.schedule_id
-      cs.section_id = nil #??????
+      cs.section_id = section.try(:id) || course.sections.first.try(:id)
     end
   end
 end
 
-class Legacy::Importer < Legacy::Base
-  def import_all
-    false
+class Importer < ActiveRecord::Base
+  self.abstract_class = true
+  
+  def self.import_all
+    OldUser.all.each do |u|
+      p u.name
+      u.schedules.each_with_index do |s,i|
+        print " Schedule " + (i+1).to_s + ": "
+        p s.course_selections.map {|cs| 
+            model = cs.to_model if cs.scheduled_course
+            model.course.number.to_s if model
+          }.select {|n| !n.nil? }.join ", "
+      end
+    end
   end
 end
