ee64553308812e1373302392d35a7425b477161a
Jason MacDonald <jason@Jasons-MacBook-Pro.local>
2012-01-11 05:45:05 -0800

    Cancan - ability to only see users if they are friends or discoverable

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,6 +3,8 @@ class Ability
 
   def initialize(user)
     user ||= User.new
-    can :read, User, :uid => user.uid
+    can :read, User do |u|
+    	(u.id == user.id) or (user.friends.map(&:id).include? u.id) or u.discoverable?
+    end
   end
 end
