8e192983a98f75ab1315e7297ed3a2a81263b3ca
Vincent Siao <vincentsiao@gmail.com>
2012-01-17 04:23:42 -0500

    Schedule page course-info-link readded, friends sort options style

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -2,6 +2,8 @@ class FriendsController < ApplicationController
   def index   
     redirect_to root_path if !user_signed_in?
     @friends = current_user.friends
+    @curr_friends = @friends.reject {|f| f.main_schedule(current_semester).nil? }
+    @prev_friends = @friends.select {|f| f.main_schedule(current_semester).nil? }
   end
 
 end
