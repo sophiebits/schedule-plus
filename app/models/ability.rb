cdc7642d0ae9d5beea463a605a36f375b47377dc
Jason MacDonald <jason@Jasons-MacBook-Pro.local>
2012-01-17 02:29:51 -0500

    Fix for creating CourseSelections authorization

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -6,7 +6,7 @@ class Ability
     can :read, User do |u|
     	(u.id == user.id) or user.friends.include?(u) or u.discoverable?
     end
-    can [:create, :update, :destroy], CourseSelection do |cs|
+    can [:update, :destroy], CourseSelection do |cs|
       cs.schedule.user == user
     end
   end
