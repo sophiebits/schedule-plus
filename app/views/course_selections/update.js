0afe662458348acda2ab9dca82ca1b3ffbd84e1b
Jason MacDonald <jason@Jasons-MacBook-Pro.local>
2012-01-18 01:27:48 -0500

    Adding back in code to change calendar with js on course selection change

--- app/views/course_selections/update.js
+++ app/views/course_selections/update.js
@@ -1,6 +1,14 @@
+$('#calendar').find('.course<%= @selection.section.course.number %>').remove();
+var course = $('#schedule .course<%= @selection.section.course.number %>');
+course.find('.selected_section').html('<%= @selection.section.letter %>');
+course.find('.sections .selected').removeClass('selected');
+course.find('.sections #section<%= @selection.section.id %>').addClass('selected');
+Calendar.addCourse(course);
+var days = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday'];
+for (var i = 0; i < days.length; ++i)
+  Calendar.layoutDay($('#main-schedule li.' + days[i] + ' .courses li'));
+  
 course_box = $('#course<%= @selection.course.number %>');
 updated_course = "<%= escape_javascript(render :partial => 'schedules/course', :locals => {:cs => @selection} )%>";
 course_box.html($(updated_course).html());
 course_box.find('.friends .facebox, .friends .section-header').hide();
-
-
