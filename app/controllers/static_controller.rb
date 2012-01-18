a743304d321acb251adc93ce1e7fec597afcf8b1
Vincent Siao <vincentsiao@gmail.com>
2012-01-18 01:11:19 -0500

    Hotfix for 500 error on freezing-dusk

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -2,8 +2,8 @@ class StaticController < ApplicationController
 
   def home
     if user_signed_in?
-      if current_user.main_schedule(current_semester)
-        redirect_to schedule_path(current_user.main_schedule(current_semester))
+      if current_user.main_schedule(Semester.current)
+        redirect_to schedule_path(current_user.main_schedule(Semester.current))
       else
         redirect_to schedules_path
       end
